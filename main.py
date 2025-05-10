a8d7d9e5f4c6010053dc27f47d05036ad763c74a
adinak <adinak057@gmail.com>
2025-05-10 09:56:34 +0600

    rectangle

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 import random
 random #librery
 # 1. Создание списка из случайных чисел от 1 до 100 и вывести только чётные.
-import random
-list = []
-for i in range(100):
-    r = random.randint(1, 100) 
-    if(r%2==0):
-     list.append(r)
-print(list)
+# import random
+# list = []
+# for i in range(100):
+#     r = random.randint(1, 100) 
+#     if(r%2==0):
+#      list.append(r)
+# print(list)
 
 # Пользователь вводит слово. Определи, является ли оно палиндромом.
-userWord=input("enter some word : ")
-if userWord == userWord[::-1]:
-    print("Это палиндром!")
-else:
-    print("Это не палиндром.")
+# userWord=input("enter some word : ")
+# if userWord == userWord[::-1]:
+#     print("Это палиндром!")
+# else:
+#     print("Это не палиндром.")
 
 
 # ✅ Задание 3: Найди сумму всех чисел в списке
-Sumalist=[3,76,9,212]
-total=0
-for i in list:
-   total+=i
-print("Сумма всех чисел:", total)
+# Sumalist=[3,76,9,212]
+# total=0
+# for i in list:
+#    total+=i
+# print("Сумма всех чисел:", total)
 
 #✅ Task 4: Count the number of vowels in a word
 
-word = input("Enter a word: ").lower()
-vowels = "aeiouAEIOU"
-count = 0
-for letter in word:
-    if letter in vowels:
-        count += 1
-print("Number of vowels:", count)
+# word = input("Enter a word: ").lower()
+# vowels = "aeiouAEIOU"
+# count = 0
+# for letter in word:
+#     if letter in vowels:
+#         count += 1
+# print("Number of vowels:", count)
+
+
+#✅ Task: Create a Person class
+# class Person:
+#    def __init__(self,name ,age):
+#       self.name=name
+#       self.age=age
+#    def greet(self):
+#       print("Hello my name is",self.name,".And I'm",self.age,"years old.")
+# p=Person("Aliya",12)
+# p.greet()
+      
+
+# ✅ Task 2: Class Rectangle
+
+      
